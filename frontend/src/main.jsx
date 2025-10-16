52caea8b0d1185925fee18ab7505714311f1e898
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2025-10-16 18:42:27 +0530

    Fix: add .jsx extension in import

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import { BrowserRouter } from "react-router";
 import { Toaster } from 'react-hot-toast';
 import { UserProvider } from './context/UserContext.jsx';
-import App from './App';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
