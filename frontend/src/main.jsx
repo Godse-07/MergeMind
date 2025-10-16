2a846d05a70d363b59bc8f87eca308c6644964fc
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2025-10-16 18:39:21 +0530

    update naming convention

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter } from "react-router";
-import App from './App.jsx'
 import { Toaster } from 'react-hot-toast';
 import { UserProvider } from './context/UserContext.jsx';
+import App from './App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
