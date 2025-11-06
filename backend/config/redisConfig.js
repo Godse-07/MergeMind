68b4d01a3bf5fe9fd7b537991b8f9570bed25f70
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2025-11-06 17:50:53 +0530

    updated redis config

--- backend/config/redisConfig.js
+++ backend/config/redisConfig.js
@@ -8,30 +8,11 @@ const redisConfig = isProd
       port: process.env.REDIS_PORT,
       username: process.env.REDIS_USERNAME,
       password: process.env.REDIS_PASSWORD,
-      tls: {
-        servername: process.env.REDIS_HOST,
-        rejectUnauthorized: false,
-      },
-
-      maxRetriesPerRequest: null,
-      reconnectOnError: (err) => {
-        console.error("🔁 Redis Cloud reconnecting after error:", err.message);
-        return true;
-      },
-      retryStrategy(times) {
-        const delay = Math.min(times * 200, 5000);
-        console.log(`⏳ Reconnecting to Redis Cloud in ${delay}ms`);
-        return delay;
-      },
+      tls: { servername: process.env.REDIS_HOST },
     }
   : {
       host: process.env.REDIS_HOST || "127.0.0.1",
       port: process.env.REDIS_PORT || 6379,
-      retryStrategy(times) {
-        const delay = Math.min(times * 200, 5000);
-        console.log(`⏳ Reconnecting to Local Redis in ${delay}ms`);
-        return delay;
-      },
     };
 
 module.exports = redisConfig;
