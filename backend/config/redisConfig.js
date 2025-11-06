218676e09e7c9cf82d74db7a5015ed1f9e83b255
Pushan Mukhopadhyay <mukhopadhyaypushan42@gmail.com>
2025-11-06 17:22:16 +0530

    updated redis config

--- backend/config/redisConfig.js
+++ backend/config/redisConfig.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 
 const isProd = process.env.NODE_ENV === "production";
 
-const redisConfig = {
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-  username: process.env.REDIS_USERNAME || undefined,
-  password: process.env.REDIS_PASSWORD || undefined,
-  ...(isProd ? { tls: { rejectUnauthorized: false } } : {}),
-};
+const redisConfig = isProd
+  ? {
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      username: process.env.REDIS_USERNAME,
+      password: process.env.REDIS_PASSWORD,
+      tls: {
+        servername: process.env.REDIS_HOST,
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      host: process.env.REDIS_HOST || "127.0.0.1",
+      port: process.env.REDIS_PORT || 6379,
+    };
 
 module.exports = redisConfig;
